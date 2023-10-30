ffc5afebb068e8925c73a6901ab3ab76d42da798
Admin <Admin@DESKTOP-88KJRN4>
2023-10-30 21:42:18 +0530

    First commit

---
+++ src/gayathri/dinesh.java
@@ -0,0 +1,10 @@
+package gayathri;
+
+public class dinesh {
+	public static void main(String[] args) {
+		System.out.println("gayathri weds dinesh");
+		System.out.println("Dinesh Love Gayathri");
+		System.out.println("All are fair in life");
+	}
+
+}
